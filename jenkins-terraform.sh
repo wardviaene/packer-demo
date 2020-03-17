79cde4c04c455f2260cdeaa3a17dc77d622a6fad
Edward Viaene <ward@in4it.io>
2020-03-17 12:12:04 +0100

    added region

--- jenkins-terraform.sh
+++ jenkins-terraform.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+AWS_REGION="eu-west-1"
+
 ARTIFACT=`packer build -machine-readable packer-demo.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 echo "packer output:"
 cat packer-demo.json
@@ -10,5 +12,5 @@ echo "AMI ID: ${AMI_ID}"
 
 echo "writing amivar.tf and uploading it to s3"
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
-S3_BUCKET=`aws s3 ls |grep terraform-state |tail -n1 |cut -d ' ' -f3`
-aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
