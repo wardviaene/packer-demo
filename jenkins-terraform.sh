221cdf4b555d9ab1aa7c51b740d39846c1eb4290
Edward Viaene <ward@in4it.io>
2020-03-17 11:06:13 +0100

    jenkins terraform script

---
+++ jenkins-terraform.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ARTIFACT=`packer build -machine-readable packer-demo.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+echo "packer output:"
+cat packer-demo.json
+
+AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+echo "AMI ID: ${AMI_ID}"
+
+echo "writing amivar.tf and uploading it to s3"
+echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
+S3_BUCKET=`aws s3 ls |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf
