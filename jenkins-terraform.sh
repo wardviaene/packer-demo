9d9bb952b8047fc26eedb0c5a92ca596f632f897
Japneet Sahni <japneetsahni30@gmail.com>
2020-06-10 12:18:27 +0530

    changes to region and folder

--- jenkins-terraform.sh
+++ jenkins-terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-AWS_REGION="eu-west-1"
+AWS_REGION="us-east-1"
 
 ARTIFACT=`packer build -machine-readable packer-demo.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 echo "packer output:"
